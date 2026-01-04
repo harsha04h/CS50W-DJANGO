fdef51a0ede7e074429ca4c180b7297f0778486e
harsha04h <harsha004h@gmail.com>
2026-01-04 21:01:56 +0530

    chngs

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, World!")
+
+def harsha(request):
+    return HttpResponse("Hello, Harsha!")
