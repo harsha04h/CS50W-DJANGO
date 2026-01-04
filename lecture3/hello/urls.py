d36d9b01f3f29381d9f72cd06c789adfed59dbc6
harsha04h <harsha004h@gmail.com>
2026-01-04 21:45:34 +0530

    added comma(error resolved)

--- lecture3/hello/urls.py
+++ lecture3/hello/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("",views.index,name="index"),
-    path("harsha",views.harsha,name="harsha")
+    path("harsha",views.harsha,name="harsha"),
     path("<str:name>",views.greet,name="greet"),
 ]
